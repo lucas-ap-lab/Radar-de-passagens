7a52d07a5eaba1abe7f96b572054038ef2f3c2a1
lucas-ap-lab <lucas-ap@hotmail.com>
2025-10-31 13:41:28 -0300

    Update next.config.js
    /** @type {import('next').NextConfig} */
    const nextConfig = {
      reactStrictMode: true,
      env: {
        AMADEUS_API_KEY: process.env.AMADEUS_API_KEY,
        AMADEUS_API_SECRET: process.env.AMADEUS_API_SECRET,
      },
    };
    
    module.exports = nextConfig;

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    AMADEUS_API_KEY: process.env.AMADEUS_API_KEY,
+    AMADEUS_API_SECRET: process.env.AMADEUS_API_SECRET,
+  },
+};
+
+module.exports = nextConfig;
